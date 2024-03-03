a98d9e4dc6c6305ada4df1ddfa96c3f9afb0dd4f
DavidDvirFolder <davidprojectfolder@gmail.com>
2024-03-03 08:07:52 +0200

    Hangman added word list from 03.03.24

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,16 @@
 import random
 
 display = []
-word_list = ["apple"]
-random_word = random.choice(word_list)
+word_list = ["Apple", "Banana", "Orange", "Mango", "Strawberry",
+             "Pineapple", "Watermelon", "Avocado", "Cherry", "Blueberry",
+             "Raspberry", "Grapefruit", "Kiwi", "Pomegranate", "Blackberry",
+             "Peach", "Plum", "Apricot", "Lemon", "Lime",
+             "Coconut", "Fig", "Papaya", "Nectarine", "Grape",
+             "Cantaloupe", "Honeydew", "Cranberry", "Elderberry"]
+random_word = random.choice(word_list).lower()
 word_length = len(random_word)
-print(random_word)
-match_found = False
+print("Welcome to Hangman!")
+end_of_game = False
 
 for i in range(word_length):
     display += "_"
@@ -13,20 +18,21 @@ print(display)
 
 while "_" in display and word_length > 0:
     guess = input("Guess a letter: \n").lower()
-    match_found = False  # Flag to check if a match is found
+    end_of_game = False  # Flag to check if a match is found
     for i in range(word_length):
         letter = random_word[i]
         if letter == guess:
             display[i] = letter
-            match_found = True
+            end_of_game = True
         if not ("_" in display):
-            print(f"Boom! , the word is:")
+            print(f"Congratulations! You guessed the word:")
             break
-    if not match_found:
-        print(f"You have more {word_length} attempts!")
+    if not end_of_game:
+        print(f"Incorrect guess.")
         word_length -= 1
+        print(f"Attempts left: {word_length}")
     if word_length == 0:
-        print("Game Over")
+        print(f"Sorry, you've run out of attempts. The correct word was: {random_word}")
         break
 
     print("".join(display))
